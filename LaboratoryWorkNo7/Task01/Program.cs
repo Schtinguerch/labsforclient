daae9ccea6405758096941742a3b40929cb6fbeb
Schtinguerch <schtinguerch@gmail.com>
2021-11-30 13:29:02 +0500

    Laboratory Work No 6 and 7 done

---
+++ LaboratoryWorkNo7/Task01/Program.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using static System.Console;
+
+namespace Task01
+{
+    class Program
+    {
+        class StudentGroup
+        {
+            public string CodeName;
+
+            public Student[] Students;
+
+            public StudentGroup(string name, Student[] students)
+            {
+                CodeName = name;
+                Students = students;
+            }
+
+            public double AverageMark()
+            {
+                double markSum = 0;
+
+                for (int i = 0; i < Students.Length; i++)
+                {
+                    markSum += Students[i].AverageMark();
+                }
+
+                double average = markSum / Students.Length;
+                return average;
+            }
+
+            public string Info()
+            {
+                var info = $"Группа {CodeName}: Ср. балл = {AverageMark()}\n";
+
+                for (int i = 0; i < Students.Length; i++)
+                {
+                    info += $"Студент №{i + 1}: {Students[i].Info()}\n";
+                }
+
+                return info;
+            }
+        }
+
+        class Student
+        {
+            public string Name;
+
+            public int[] Marks;
+
+            public Student(string name, int[] marks)
+            {
+                Name = name;
+                Marks = marks;
+            }
+
+            public double AverageMark()
+            {
+                int markSum = 0;
+
+                for (int i = 0; i < Marks.Length; i++)
+                {
+                    markSum += Marks[i];
+                }
+
+                double averageMark = (double)markSum / Marks.Length;
+                return averageMark;
+            }
+
+            public string Info()
+            {
+                var info = $"{Name}: ср. оц. = {AverageMark()}; все оценки: ";
+                info += Marks[0].ToString();
+
+                for (int i = 1; i < Marks.Length; i++)
+                {
+                    info += $", {Marks[i]}";
+                }
+
+                return info;
+            }
+        }
+
+        class MasterDegreeStudent : Student
+        {
+            public string GraduationWorkName;
+
+            public MasterDegreeStudent(string name, int[] marks, string graduationWorkName) : base(name, marks)
+            {
+                GraduationWorkName = graduationWorkName;
+            }
+
+            public string Info()
+            {
+                return base.Info() + $"; Вып. работа: {GraduationWorkName}";
+            }
+        }
+
+        static string[] _names = new string[]
+        {
+            "Михаил  ",
+            "Татьяна ",
+            "Алексей ",
+            "Виктория",
+            "Виталий ",
+            "Азамат  ",
+            "Любава  ",
+        };
+
+        static string[] _works = new string[]
+        {
+            "Новые методы ввода текста",
+            "Ускорение обучения школьников",
+            "Сравнительный анализ раскладок",
+            "Новое решение тепловых задач",
+            "Язык программирования фотонных ВМ",
+        };
+
+        static Random _random = new Random();
+
+        static int MinValue = 1;
+        static int MaxValue = 5 + 1; //Random.Next не включает последее число
+
+        static void Main(string[] args)
+        {
+            var studentGroups = UserGroups(3);
+
+            SortByAverageMark(ref studentGroups);
+            PrintGroups(studentGroups, "\n\nГруппы, упорядоченные по успеваемости");
+        }
+
+        static void SortByAverageMark(ref StudentGroup[] groups)
+        {
+            for (int i = 0; i < groups.Length; i++)
+            {
+                for (int j = 0; j < groups.Length - 1; j++)
+                {
+                    if (groups[j].AverageMark() < groups[j + 1].AverageMark())
+                    {
+                        var temp = groups[j + 1];
+                        groups[j + 1] = groups[j];
+                        groups[j] = temp;
+                    }
+                }
+            }
+        }
+
+        static StudentGroup[] UserGroups(int count)
+        {
+            var groups = new StudentGroup[count];
+
+            for (int i = 0; i < count - 1; i++)
+            {
+                WriteLine($"Группа №{i + 1}:");
+
+                Write($"Название группы: ");
+                var groupName = ReadLine();
+                var students = RandomUserStudents();
+
+                groups[i] = new StudentGroup(groupName, students);
+            }
+
+            groups[count - 1] = new StudentGroup("Супер-магистры", RandomUserMasters());
+            return groups;
+        }
+
+        static Student[] RandomUserStudents()
+        {
+            Write("Введите кол-во студентов: ");
+            int studentCount = int.Parse(ReadLine());
+
+            var students = new Student[studentCount];
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                students[i] = RandomStudent();
+            }
+
+            return students;
+        }
+
+        static MasterDegreeStudent[] RandomUserMasters()
+        {
+            Write("Введите кол-во студентов-магистров: ");
+            int studentCount = int.Parse(ReadLine());
+
+            var students = new MasterDegreeStudent[studentCount];
+
+            for (int i = 0; i < studentCount; i++)
+            {
+                students[i] = RandomMasterStudent();
+            }
+
+            return students;
+        }
+
+        static Student RandomStudent()
+        {
+            var name = _names[_random.Next(_names.Length)];
+            var marks = new int[4];
+
+            for (int i = 0; i < marks.Length; i++)
+            {
+                marks[i] = _random.Next(MinValue, MaxValue);
+            }
+
+            return new Student(name, marks);
+        }
+
+        static MasterDegreeStudent RandomMasterStudent()
+        {
+            var name = _names[_random.Next(_names.Length)];
+            var work = _works[_random.Next(_works.Length)];
+            var marks = new int[4];
+
+            for (int i = 0; i < marks.Length; i++)
+            {
+                marks[i] = _random.Next(MinValue, MaxValue);
+            }
+
+            return new MasterDegreeStudent(name, marks, work);
+        }
+
+        static void PrintGroups(StudentGroup[] groups, string message = "")
+        {
+            if (message != "")
+            {
+                WriteLine(message);
+            }
+
+            if (groups.Length == 0)
+            {
+                WriteLine("* Массив пуст *");
+                return;
+            }
+
+            for (int i = 0; i < groups.Length; i++)
+            {
+                WriteLine($"{i + 1}: {groups[i].Info()}");
+            }
+        }
+    }
+}
---
+++ LaboratoryWorkNo7/Task02/Program.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using static System.Console;
+
+namespace Task02
+{
+    class Program
+    {
+        class MatchResult
+        {
+            public string FirstTeam;
+            public string SecondTeam;
+
+            public int FirstTeamGoals;
+            public int SecondTeamGoals;
+
+            public MatchResult(string firstTeam, string secondTeam, int firstTeamGoals, int secondTeamGoals)
+            {
+                FirstTeam = firstTeam;
+                SecondTeam = secondTeam;
+
+                FirstTeamGoals = firstTeamGoals;
+                SecondTeamGoals = secondTeamGoals;
+            }
+
+            public string Info()
+            {
+                return $" {FirstTeam} | {FirstTeamGoals} : {SecondTeamGoals} | {SecondTeam} ";
+            }
+        }
+
+        class Team
+        {
+            public string Name;
+            public string CodeName;
+        }
+
+        class FootballTeam : Team
+        {
+            public int EarnedPoints;
+            public int GoalsDifference;
+
+            public FootballTeam(string name, string codeName)
+            {
+                Name = name;
+                CodeName = codeName;
+
+                EarnedPoints = 0;
+                GoalsDifference = 0;
+            }
+
+            public bool IsStronger(FootballTeam otherTeam)
+            {
+                if (EarnedPoints == otherTeam.EarnedPoints)
+                {
+                    return GoalsDifference > otherTeam.GoalsDifference;
+                }
+
+                else
+                {
+                    return EarnedPoints > otherTeam.EarnedPoints;
+                }
+            }
+
+            public string Info()
+            {
+                return $"{Name} ({CodeName}): Очков = {EarnedPoints}; Г-П = {GoalsDifference}";
+            }
+        }
+
+        static Random _random = new Random();
+        static int MinGoals = 0;
+        static int MaxGoals = 10;
+
+        static string[] _teamNames = new string[]
+        {
+            "Manchester United",
+            "Real Madrid      ",
+            "Barcelona        ",
+            "Uvensus          ",
+            "Arsenal          ",
+        };
+
+        static void Main(string[] args)
+        {
+            var teams = DeclareAllTeams();
+            CreateRandomTournament(ref teams);
+
+            SortByEarnedPoints(ref teams);
+            PrintTeams(teams, "\nИтоговая таблица:");
+        }
+
+        static void SortByEarnedPoints(ref FootballTeam[] teams)
+        {
+            for (int i = 0; i < teams.Length; i++)
+            {
+                for (int j = 0; j < teams.Length - 1; j++)
+                {
+                    if (!teams[j].IsStronger(teams[j + 1]))
+                    {
+                        var temp = teams[j + 1];
+                        teams[j + 1] = teams[j];
+                        teams[j] = temp;
+                    }
+                }
+            }
+        }
+
+        static void CreateRandomTournament(ref FootballTeam[] teams)
+        {
+            const int matchCount = 16;
+
+            for (int i = 0; i < matchCount; i++)
+            {
+                int firstTeamGoals = _random.Next(MinGoals, MaxGoals);
+                int secondTeamGoals = _random.Next(MinGoals, MaxGoals);
+
+                int firstTeam = _random.Next(_teamNames.Length);
+                int secondTeam = _random.Next(_teamNames.Length);
+
+                while (secondTeam == firstTeam)
+                {
+                    secondTeam = _random.Next(_teamNames.Length);
+                }
+
+                WriteLine(new MatchResult(
+                    _teamNames[firstTeam],
+                    _teamNames[secondTeam],
+                    firstTeamGoals,
+                    secondTeamGoals).Info());
+
+                teams[firstTeam].GoalsDifference += firstTeamGoals - secondTeamGoals;
+                teams[secondTeam].GoalsDifference += secondTeamGoals - firstTeamGoals;
+
+                if (firstTeamGoals > secondTeamGoals)
+                {
+                    teams[firstTeam].EarnedPoints += 3;
+                }
+
+                else if (secondTeamGoals > firstTeamGoals)
+                {
+                    teams[secondTeam].EarnedPoints += 3;
+                }
+
+                else
+                {
+                    teams[firstTeam].EarnedPoints += 1;
+                    teams[secondTeam].EarnedPoints += 1;
+                }
+            }
+        }
+
+        static FootballTeam[] DeclareAllTeams()
+        {
+            var teams = new FootballTeam[_teamNames.Length];
+
+            for (int i = 0; i < teams.Length; i++)
+            {
+                teams[i] = new FootballTeam(_teamNames[i], _random.Next(0, 100).ToString("0000"));
+            }
+
+            return teams;
+        }
+
+        static void PrintTeams(FootballTeam[] teams, string message = "")
+        {
+            if (message != "")
+            {
+                WriteLine(message);
+            }
+
+            if (teams.Length == 0)
+            {
+                WriteLine("* Массив пуст *");
+            }
+
+            for (int i = 0; i < teams.Length; i++)
+            {
+                WriteLine($"{i + 1}-е место: " + teams[i].Info());
+            }
+        }
+    }
+}
